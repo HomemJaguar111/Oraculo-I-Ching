ae4b3856631ac80745b752cccc9ce1ae049ba280
João Victor <95556375+HomemJaguar111@users.noreply.github.com>
2022-04-12 18:57:51 -0300

    Continua...
    consertar o resultado yin yang das moedas

--- oraculo.js
+++ oraculo.js
@@ -1,97 +1,128 @@
-let gen 
-let gen1
-let gen2
-let gen3
-let gen4
-let gen5
-let gen6 
+let md1 = null;
+let md2 = null;
+let md3 = null;
+let gen = null;
 
-let tentativa = 0 
+let tentativa = 0;
 
-let trigrama1 = [gen1, ]
+let trigrama1 = [];
 
-let trigrama2 = []
+let trigrama2 = [];
 
-let hexagrama = []
+let hexagrama = [];
 
+let triCalc = md1 + md2 + md3;
 
+let tri1_1 = document.getElementById("tri1_1");
+let tri1_2 = document.getElementById("tri1_2");
+let tri1_3 = document.getElementById("tri1_3");
 
+let tri2_1 = document.getElementById("tri2_1");
+let tri2_2 = document.getElementById("tri2_2");
+let tri2_3 = document.getElementById("tri2_3");
 
-function iching(){
-        if (tentativa == 0){
-                alert('Clique em jogar para sortear as moedas')
+let tri3_1 = document.getElementById("tri3_1");
+let tri3_2 = document.getElementById("tri3_2");
+let tri3_3 = document.getElementById("tri3_3");
 
+let tri4_1 = document.getElementById("tri4_1");
+let tri4_2 = document.getElementById("tri4_2");
+let tri4_3 = document.getElementById("tri4_3");
 
+let tri5_1 = document.getElementById("tri5_1");
+let tri5_2 = document.getElementById("tri5_2");
+let tri5_3 = document.getElementById("tri5_3");
+
+let tri6_1 = document.getElementById("tri6_1");
+let tri6_2 = document.getElementById("tri6_2");
+let tri6_3 = document.getElementById("tri6_3");
+
+function yCalc() {}
+//console.log(`A moeda 1 é ${md1}, a moeda 2 é ${md2} e a moeda 3 é ${md3}.`);
+//console.log(`A soma é ${triCalc}`);
+
+console.log("tentativa:" + tentativa);
+console.log(gen);
+
+function iching() {
+  if (tentativa == 0) {
+    alert("Clique em jogar para sortear as moedas");
+  } else if (tentativa == 1) {
+    if (gen == "yang") {
+      tri1_1.style.backgroundColor = "black";
+      tri1_2.style.backgroundColor = "black";
+      tri1_3.style.backgroundColor = "black";
+    } else {
+      tri1_1.style.backgroundColor = "black";
+      tri1_2.style.backgroundColor = "white";
+      tri1_3.style.backgroundColor = "black";
+    }
+  } else if (tentativa == 2) {
+    if (gen == "yang") {
+      tri2_1.style.backgroundColor = "black";
+      tri2_2.style.backgroundColor = "black";
+      tri2_3.style.backgroundColor = "black";
+    } else {
+      tri2_1.style.backgroundColor = "black";
+      tri2_2.style.backgroundColor = "white";
+      tri2_3.style.backgroundColor = "black";
+    }
+  } else if (tentativa == 3) {
+    if (gen == "yang") {
+      tri3_1.style.backgroundColor = "black";
+      tri3_2.style.backgroundColor = "black";
+      tri3_3.style.backgroundColor = "black";
+    } else {
+      tri3_1.style.backgroundColor = "black";
+      tri3_2.style.backgroundColor = "white";
+      tri3_3.style.backgroundColor = "black";
+    }
+  }
 }
 
+function jogar() {
+  let moeda1 = document.getElementById("moeda1");
+  let moeda2 = document.getElementById("moeda2");
+  let moeda3 = document.getElementById("moeda3");
 
+  let sort1 = Math.floor(Math.random() * 10);
+  let sort2 = Math.floor(Math.random() * 10);
+  let sort3 = Math.floor(Math.random() * 10);
+
+  if (sort1 % 2 == 0) {
+    md1 = 2;
+  } else {
+    md1 = 3;
+  }
+
+  moeda1.innerHTML = md1;
+
+  if (sort2 % 2 == 0) {
+    md2 = 2;
+  } else {
+    md2 = 3;
+  }
+
+  moeda2.innerHTML = md2;
+
+  if (sort3 % 2 == 0) {
+    md3 = 2;
+  } else {
+    md3 = 3;
+  }
+
+  moeda3.innerHTML = md3;
+
+  if (triCalc.value % 2 === 1) {
+    gen = "yin";
+  } else {
+    gen = "yang";
+  }
+
+  tentativa++;
+  console.log("tentativa " + tentativa);
+  console.log(triCalc.type);
+  console.log("gen " + gen);
+
+  return iching();
 }
-
-
-
- 
-
-function jogar() { 
-        
-        let moeda1 = document.getElementById('moeda1')
-        let moeda2 = document.getElementById('moeda2')
-        let moeda3 = document.getElementById('moeda3')
-
-    let sort1 =  Math.floor(Math.random()*10)
-    let sort2 =  Math.floor(Math.random()*10)
-    let sort3 =  Math.floor(Math.random()*10)  
-    
-    let md1 = 0
-    let md2 = 0
-    let md3 = 0
-        
-    if (sort1 % 2 == 0){
-         md1 = 2
-    }else {md1=3}
-
-        console.log('numero sorteado '+sort1)
-        console.log('restante da divisão '+(sort1 % 2))
-        moeda1.innerHTML = md1
-
-    if (sort2 % 2 == 0){
-            md2 = 2
-    }else {md2=3}
-           console.log('numero sorteado '+sort2)
-           console.log('restante da divisão '+(sort2 % 2))
-           moeda2.innerHTML = md2
-
-    if (sort3 % 2 == 0){
-            md3 = 2
-    }else {md3=3}
-
-           console.log('numero sorteado '+sort3)
-           console.log('restante da divisão '+(sort3 % 2))  
-
-           moeda3.innerHTML = md3
-           
-           triCalc = (md1+md2+ md3)
-                
-           
-                if (triCalc % 2 === 0){
-                gen ='yin'
-                } else{ gen = 'yang'}
-                gen1=gen
-
-               
-           console.log(`A moeda 1 é ${md1}, a moeda 2 é ${md2} e a moeda 3 é ${md3}.`)
-           console.log(`A soma é ${triCalc}`)
-           
-           tentativa++
-           console.log("tentativa " + tentativa)
-
-           console.log(gen)
-           console.log(gen1)
-
-           if (tentativa ==1){
-
-
-           }
-
-
-
-        }
