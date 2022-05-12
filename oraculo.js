ddcec7f95a8d2c987e589d1fb239a5cc2b43e9d6
João Victor <95556375+HomemJaguar111@users.noreply.github.com>
2022-05-12 04:22:19 -0300

    falta pouco

--- oraculo.js
+++ oraculo.js
@@ -13,6 +13,8 @@ let gen6 = null
 let trigrama1 = null
 let trigrama2 = null
 
+let hexagrama = null 
+
 let tentativa = 0
 
 let tri1_1 = document.getElementById('tri1_1')
@@ -38,6 +40,9 @@ let tri5_3 = document.getElementById('tri5_3')
 let tri6_1 = document.getElementById('tri6_1')
 let tri6_2 = document.getElementById('tri6_2')
 let tri6_3 = document.getElementById('tri6_3')
+
+let titulo = null
+let texto = null
 
 //console.log(`A moeda 1 é ${md1}, a moeda 2 é ${md2} e a moeda 3 é ${md3}.`);
 //console.log(`A soma é ${triCalc}`);
@@ -180,15 +185,7 @@ function iching() {
       gen6 = 'yin'
     }
 
-    console.log('gen ' + gen)
-    console.log('Sorteio 1 = ' + gen1)
-    console.log('Sorteio 2 = ' + gen2)
-    console.log('Sorteio 3 = ' + gen3)
-    console.log('Trigrama 1 = ' + trigrama1)
-    console.log('Sorteio 4 = ' + gen4)
-    console.log('Sorteio 5 = ' + gen5)
-    console.log('Sorteio 6 = ' + gen6)
-
+    
     if (gen4 === 'yang' && gen5 === 'yang' && gen6 === 'yang') {
       trigrama2 = 'ceu'
     } else if (gen4 === 'yang' && gen5 === 'yang' && gen6 === 'yin') {
@@ -200,19 +197,230 @@ function iching() {
     } else if (gen4 === 'yin' && gen5 === 'yin' && gen6 === 'yin') {
       trigrama2 = 'terra'
     } else if (gen4 === 'yin' && gen5 === 'yin' && gen6 === 'yang') {
-      trigrama2 = 'motanha'
+      trigrama2 = 'montanha'
     } else if (gen4 === 'yin' && gen5 === 'yang' && gen6 === 'yang') {
       trigrama2 = 'vento'
     } else if (gen4 === 'yin' && gen5 === 'yang' && gen6 === 'yin') {
       trigrama2 = 'agua'
     }
-    console.log('Trigrama 2 = ' + trigrama2)    
-  }
+oraculo()
 
-  switch(){
+
+
+
+    console.log('gen ' + gen)
+    console.log('Sorteio 1 = ' + gen1)
+    console.log('Sorteio 2 = ' + gen2)
+    console.log('Sorteio 3 = ' + gen3)
+    console.log('Trigrama 1 = ' + trigrama1)
+    console.log('Sorteio 4 = ' + gen4)
+    console.log('Sorteio 5 = ' + gen5)
+    console.log('Sorteio 6 = ' + gen6)
+    console.log('Trigrama 2 = ' + trigrama2) 
+      
     
   }
+
+ 
 }
+function oraculo(){
+  if (trigrama1==='ceu' && trigrama2==='ceu'){
+    //Hexagrama 1 - 
+      hexagrama=1 
+    }else if (trigrama1==='terra' && trigrama2==='terra'){
+        //Hexagrama 2 -   
+      hexagrama=2
+    }else if (trigrama1==='trovao' && trigrama2==='agua'){
+        //Hexagrama 3 - 
+      hexagrama=3
+    }else if (trigrama1==='agua' && trigrama2=== 'montanha'){
+        //Hexagrama 4 -   
+      hexagrama=4
+    }else if (trigrama1==='ceu' && trigrama2==='agua'){
+      //Hexagrama 5 - 
+      hexagrama=5
+    }else if (trigrama1==='agua' && trigrama2==='ceu'){
+      //Hexagrama 6 -   
+      hexagrama=6
+    }else if (trigrama1==='agua' && trigrama2==='terra'){
+      //Hexagrama 7 - 
+      hexagrama=7
+    }else if (trigama1==='terra' &&  trigrama2==='agua'){
+      //Hexagrama 8 -   
+      hexagrama=8
+    }else if ( trigrama1=== 'ceu' &&  trigrama2==='vento'){
+      //Hexagrama 9 - 
+      hexagrama=9
+    }else if(trigrama1==='lago' &&  trigrama2==='ceu'){
+      //Hexagrama 10 -   
+      hexagrama=10
+    }else if(trigrama1==='ceu' &&  trigrama2==='terra'){
+      //Hexagrama 11 - 
+      hexagrama=11
+    }else if(trigrama1==='terra' && trigrama2=== 'ceu'){
+      //Hexagrama 12 -   
+      hexagrama=12
+    }else if(trigrama1==='fogo' && trigrama2==='ceu'){
+      //Hexagrama 13 - 
+      hexagrama=13
+    }else if(trigrama1==='ceu' && trigrama2===  'fogo'){
+      //Hexagrama 14 -   
+      hexagrama=14
+    }else if(trigrama1==='montanha' &&  trigrama2==='terra'){
+      //Hexagrama 15 - 
+      hexagrama=15
+    }else if(trigrama1==='terra' && trigrama2=== 'trovao'){
+      //Hexagrama 16 -   
+      hexagrama=16
+    }else if(trigrama1==='trovao' && trigrama2=== 'lago'){
+      //Hexagrama 17 - 
+      hexagrama=17
+    }else if(trigrama1==='vento' && trigrama2=== 'montanha'){
+      //Hexagrama 18 -   
+      hexagrama=18
+    }else if(trigrama1==='lago' && trigrama2=== 'terra'){
+      //Hexagrama 19 - 
+      hexagrama=19
+    }else if(trigrama1==='terra' && trigrama2=== 'vento'){
+      //Hexagrama 20 -   e
+  hexagrama=20
+    }else if(trigrama1==='trovao' && trigrama2=== 'fogo'){
+     //Hexagrama 21 - 
+     hexagrama=21 
+    }else if(trigrama1==='fogo' && trigrama2=== 'montanha'){
+      //Hexagrama 22 -   
+       hexagrama=22
+    }else if(trigrama1==='terra' && trigrama2===  'montanha'){
+      //Hexagrama 23 - 
+      hexagrama=23
+    }else if(trigrama1==='trovao' &&  trigrama2==='terra'){
+      //Hexagrama 24 -   
+      hexagrama=24
+    }else if(trigrama1==='trovao' && trigrama2=== 'ceu'){
+      //Hexagrama 25 - 
+      hexagrama=25
+    }else if(trigrama1==='ceu' && trigrama2=== 'montanha'){
+      //Hexagrama 26 -   
+      hexagrama=26
+    }else if(trigrama1==='trovao' && trigrama2=== 'montanha'){
+      //Hexagrama 27 - 
+      hexagrama=27
+    }else if(trigrama1==='vento' && trigrama2==='lago'){
+      //Hexagrama 28 -   
+      hexagrama=28
+    }else if(trigrama1==='agua' && trigrama2=== 'agua'){
+      //Hexagrama 29 - 
+      hexagrama=29
+    }else if(trigrama1==='fogo' && trigrama2==='fogo'){
+      //Hexagrama 30 -   
+      hexagrama=30
+    }else if(trigrama1==='montanha' && trigrama2==='lago'){
+      //Hexagrama 31 - 
+     hexagrama=31
+    }else if(trigrama1==='vento' && trigrama2=== 'trovao'){
+       //Hexagrama 32 -   
+       hexagrama=32
+    }else if(trigrama1==='montanha' && trigrama2===  'ceu'){
+      //Hexagrama 33 - 
+      hexagrama=33
+    }else if(trigrama1==='ceu' && trigrama2=== 'trovao'){
+      //Hexagrama 34 -   
+      hexagrama=34
+    }else if(trigrama1==='ceu' && trigrama2==='fogo'){
+      //Hexagrama 35 - 
+      hexagrama=35
+    }else if(trigrama1==='fogo' && trigrama2=== 'terra'){
+      //Hexagrama 36 -   
+      hexagrama=36
+    }else if(trigrama1==='fogo' && trigrama2==='vento'){
+      //Hexagrama 37 - 
+      hexagrama=37
+    }else if(trigrama1==='lago' && trigrama2=== 'fogo'){
+      //Hexagrama 38 -   
+      hexagrama=38
+    }else if(trigrama1==='montanha' && trigrama2=== 'agua'){
+      //Hexagrama 39 - 
+      hexagrama=39
+    }else if(trigrama1==='agua' && trigrama2=== 'trovao'){
+      //Hexagrama 40 -   
+      hexagrama=40
+    }else if(trigrama1==='lago' && trigrama2=== 'montanha'){
+      //Hexagrama 41 - 
+      hexagrama=41
+    }else if(trigrama1==='trovao' && trigrama2=== 'vento'){
+      //Hexagrama 42 -   
+      hexagrama=42
+    }else if(trigrama1==='ceu' && trigrama2==='lago'){
+      //Hexagrama 43 - 
+      hexagrama=43
+    }else if('vento' &&  'ceu'){
+      //Hexagrama 44 -   
+      hexagrama=44
+    }else if(trigrama1==='terra' && trigrama2=== 'lago'){
+      //Hexagrama 45 - 
+      hexagrama=45
+    }else if(trigrama1==='vento' && trigrama2=== 'terra'){
+      //Hexagrama 46 -   
+      hexagrama=46
+    }else if(trigrama1==='agua' && trigrama2==='lago'){
+      //Hexagrama 47 - 
+      hexagrama=47
+    }else if(trigrama1==='vento' && trigrama2==='agua'){
+      //Hexagrama 48 -   
+      hexagrama=48
+    }else if(trigrama1==='fogo' && trigrama2=== 'lago'){
+      //Hexagrama 49 - 
+      hexagrama=49
+    }else if(trigrama1==='vento' && trigrama2=== 'fogo'){
+      //Hexagrama 50 -   
+      hexagrama=50
+    }else if(trigrama1==='trovao' &&  trigrama2=== 'trovao'){
+      //Hexagrama 51 - 
+      hexagrama=51
+    }else if(trigrama1==='montanha' && trigrama2=== 'montanha'){
+      //Hexagrama 52 -   
+      hexagrama=52
+    }else if(trigrama1==='montanha' && trigrama2=== 'vento'){
+      //Hexagrama 53 - 
+      hexagrama=53
+    }else if(trigrama1==='lago' && trigrama2=== 'trovao'){
+      //Hexagrama 54 -   
+      hexagrama=54
+    }else if(trigrama1==='fogo' && trigrama2==='trovao'){
+      //Hexagrama 55 - 
+      hexagrama=55
+    }else if(trigrama1==='montanha' && trigrama2=== 'fogo'){
+      //Hexagrama 56 -   
+      hexagrama=56
+    }else if(trigrama1==='vento' && trigrama2=== 'vento'){
+      //Hexagrama 57 - 
+      hexagrama=57
+    }else if(trigrama1==='lago' && trigrama2=== 'lago'){
+      //Hexagrama 58 -   
+      hexagrama=58
+    }else if(trigrama1==='agua' && trigrama2=== 'vento'){
+      //Hexagrama 59 - 
+      hexagrama=59
+    }else if(trigrama1==='lago' && trigrama2=== 'agua'){
+      //Hexagrama 60 -   
+      hexagrama=60
+    }else if(trigrama1=== 'lago' && trigrama2=== 'vento'){
+      //Hexagrama 61 - 
+      hexagrama=61
+    }else if(trigrama1==='montanha' && trigrama2=== 'trovao'){
+      //Hexagrama 62 -   
+      hexagrama=62
+    }else if(trigrama1 ==='fogo' && trigrama2=== 'agua'){
+      //Hexagrama 63 - 
+      hexagrama=63
+    }else if(trigrama1==='agua' && trigrama2=== 'fogo'){
+      //Hexagrama 64 -   
+      hexagrama=64
+    }
+    
+     
+  }
+
 
 function jogar() {
   let moeda1 = document.getElementById('moeda1')
